8943d41a81178a4bdbd4f9d0d14bba02eabe7ea8
samritak <152491485+samritak@users.noreply.github.com>
2025-01-13 14:16:58 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,3 @@
-# Create a directory for Chromium
-mkdir -p chromium
-
-# Download the latest portable Chromium build
-wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chromium/chrome.deb
-
-# Extract the deb package (since you can't install it directly)
-dpkg-deb -x chromium/chrome.deb chromium/
-
-# Set the Chrome binary path
-mv chromium/opt/google/chrome/chrome chromium/
-
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get update
 apt-get install -y ./google-chrome-stable_current_amd64.deb
