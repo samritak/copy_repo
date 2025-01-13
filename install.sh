90d1fcad8cfccf23523f3f1a50bb26a2b4f694ab
samritak <152491485+samritak@users.noreply.github.com>
2025-01-13 14:07:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,5 +11,5 @@ dpkg-deb -x chromium/chrome.deb chromium/
 mv chromium/opt/google/chrome/chrome chromium/
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt-get update
-sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
+apt-get update
+apt-get install -y ./google-chrome-stable_current_amd64.deb
