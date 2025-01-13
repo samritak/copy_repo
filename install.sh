846a0ea94559d79811aee3c1dad39707b90d80c7
samritak <152491485+samritak@users.noreply.github.com>
2025-01-13 13:51:52 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,3 +9,7 @@ dpkg-deb -x chromium/chrome.deb chromium/
 
 # Set the Chrome binary path
 mv chromium/opt/google/chrome/chrome chromium/
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get update
+sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
