bfe4477784d93c944f5e0ce4ff956592ab02939d
samritak <152491485+samritak@users.noreply.github.com>
2025-01-13 14:18:43 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get update
-apt-get install -y ./google-chrome-stable_current_amd64.deb
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# apt-get update
+# apt-get install -y ./google-chrome-stable_current_amd64.deb
+
+wget https://chromedriver.storage.googleapis.com/<version>/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip -d chromedriver/
+export PATH=$PATH:$(pwd)/chromedriver
